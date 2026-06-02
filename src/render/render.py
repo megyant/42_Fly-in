fb30fa1416ee016fdc91292224d490c707886737
megyant <megyant@gmail.com>
2026-06-02 08:24:41 +0100

    fixed legend

--- src/render/render.py
+++ src/render/render.py
@@ -248,10 +248,10 @@ class Render:
                                              self.color_map['black'])
         self.screen.blit(text_surface, (20, self.heigth-100 - 10))
 
-        texts = ['priority',
-                 'restricted',
-                 'blocked',
-                 'normal']
+        texts = ['blocked',
+                 'normal',
+                 'priority',
+                 'restricted']
         for i, text in enumerate(texts):
             color = self.color_map.get(self.zone_color[text])
 
